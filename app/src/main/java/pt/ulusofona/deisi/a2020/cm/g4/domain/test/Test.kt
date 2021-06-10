251eea3b91792d05846b26d86f0687d7f8d3077d
catarina-21805632 <catarina.mlmoita@gmail.com>
2021-06-10 17:23:23 +0100

    já não é necessário

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g4/domain/test/Test.kt
+++
@@ -1,8 +0,0 @@
-package pt.ulusofona.deisi.a2020.cm.g4.domain.test
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-import java.io.File
-
-@Parcelize
-class Test(val date: String, val result: String, val local: String, val image: File?, val dateReg: String) : Parcelable
